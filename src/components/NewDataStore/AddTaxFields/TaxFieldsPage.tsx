70b32d622d13fb8ae68514e7825f94ef1e5abf01
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-07-06 17:09:47 +0530

    some repetitive styling..!

--- src/components/NewDataStore/AddTaxFields/TaxFieldsPage.tsx
+++ src/components/NewDataStore/AddTaxFields/TaxFieldsPage.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './TaxFieldsPage.css'
-
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
+import RemoveCircleTwoToneIcon from '@material-ui/icons/RemoveCircleTwoTone';
+import AddBoxTwoToneIcon from '@material-ui/icons/AddBoxTwoTone';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 interface Props {
     displayTaxFields : Array<string>;
     removeTaxField : (index : number) => any;
@@ -12,27 +17,43 @@ interface Props {
 
 const TaxFieldsPage : React.FC<Props> = ({displayTaxFields,removeTaxField,addTaxField,writeValue,nextPage,backPage}) => {
     return (
-        <div>
-            Tax Fields Page
-
-            {   
-            displayTaxFields.map( (taxField,index) => {
-
-            return (<div key = {index}> <input type="text" value = {taxField} onChange = {(e)=>writeValue(e.target.value,index)}  /> <span onClick = {
-                () => removeTaxField(index)
-            }>{index}-</span>
-        </div>)
-
-                })
+        <div className = "taxFieldsPageMain">
             
-        }
+            <div className="taxSideBlock">
+                <div className = "tax">Add Tax Fields</div>
+                <div className="taxInfo">You can skip this step and Add Tax Fields, even at the time of generating a Recipt!</div>
+            </div>
 
-        <div className="addDataField"><span onClick = {
+            <div className="taxFieldsDiv">
+            {   (displayTaxFields.length) ?
+                    displayTaxFields.map( (taxField,index) => {
+
+                    return (<div className = "taxFieldDiv" key = {index}> 
+
+                <Button variant="contained"  className = "taxFieldButton" >
+                        <TextField id={`standard-basic ${index}`} size = "small" value = {taxField}  onChange={(e)=>writeValue(e.target.value,index)} className = "taxField" />
+                        <span onClick = {
+                () => removeTaxField(index)
+            } ><RemoveCircleTwoToneIcon/></span>
+
+                </Button>
+                </div>)
+
+                        }) : <div className="taxInfo">Add Tax Fields</div>
+            
+            }
+
+            
+        <div className="addTaxField"><span onClick = {
             () => addTaxField()
-        } >+</span></div>
+        } ><AddBoxTwoToneIcon /></span></div>
 
-        <div onClick = {()=> nextPage()}>Next</div>
-        <div onClick = {()=> backPage()}>Back</div>
+            </div>
+            
+
+
+        <div onClick = {()=> nextPage()} className="taxNextPageButton"><ArrowForwardIcon style ={{fontSize: 30}} /></div>
+        <div onClick = {()=> backPage()} className="taxBackPageButton"><ArrowBackIcon style = {{fontSize: 30}}  /></div>
 
         </div>
     )
