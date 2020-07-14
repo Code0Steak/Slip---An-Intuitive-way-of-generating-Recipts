f5f905d08a19052aa7ea2db61d6534825c5f4d37
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-07-14 09:31:17 +0530

    small tweaks can help us go long ways

--- src/components/TabularDataComponents/ForNewData/ForDataFieldsPage/ItemsDataTable.tsx
+++ src/components/TabularDataComponents/ForNewData/ForDataFieldsPage/ItemsDataTable.tsx
@@ -53,7 +53,7 @@ const ItemsDataTable : React.FC<Props> = ({cells,rows}) => {
                 <TableHead>
                 <TableRow>
                     {
-                        cells.map((cell : string,index : number) =>{ (cell) ? <StyledTableCell key = {index}>{cell}</StyledTableCell> : ''})
+                        cells.filter((cell : string) => cell != '').map((cell : string,index : number) =><StyledTableCell key = {index}>{cell}</StyledTableCell> )
                     }
                 </TableRow>
                 </TableHead>
@@ -61,15 +61,14 @@ const ItemsDataTable : React.FC<Props> = ({cells,rows}) => {
                 {rows.map((row : any,index : number) => (
                     <TableRow key={index}>
                     {
-                      Object.keys(row).map((key: string,i : number) => 
+                      Object.keys(row).filter((key: string) => cells[parseInt(key)] !== '' ).map((key: string,i : number) => 
                       
-                        {(cells[parseInt(key)]) ?
+                        
                         <StyledTableCell align="right" key = {key}>
                           
                           <TextField id={`standard-basic ${row[key]} ${key}`} label={`Item ${index + 1} ${key}`} value = {row[key]} autoComplete = "none" key={`standard-basic ${i}`} />
       
-                          </StyledTableCell> : ''
-                          }
+                          </StyledTableCell> 
                           
                         )
                     }
